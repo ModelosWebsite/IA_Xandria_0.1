ec71e83a0850a4044ac4e09304cdea73a96c9794
Angelino-Cangato <angelyurdys@gmail.com>
2025-04-29 17:37:41 +0100

    commit2

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 # Middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://xzero.ao"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
